45148d23ca392937aaa70918c53435e70de4603e
Ryan Fauver <ryan.fauver@gmail.com>
2014-10-16 15:57:20 -0400

    add portfolio and contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get 'static_pages/home'
+  match '/contact',   to: 'static_pages#contact',   via: 'get'
+  match '/portfolio', to: 'static_pages#portfolio', via: 'get'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
