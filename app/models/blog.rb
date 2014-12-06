b7f3b992ad37e8ffdb721a9d9d5e1503ff8e28c0
Ryan Fauver <ryan.fauver@gmail.com>
2014-12-06 16:51:13 -0500

    modify header image

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,5 +1,5 @@
 class Blog < ActiveRecord::Base
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, uniqueness: true
   validates :content, presence: true
   max_paginates_per 5
